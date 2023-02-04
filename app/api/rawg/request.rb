6f5e81ba79d226516fdaa198c66c4afb254a9017
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-02-04 10:36:42 +0800

    removed 'next' and 'previous' keys from response data

--- app/api/rawg/request.rb
+++ app/api/rawg/request.rb
@@ -9,9 +9,15 @@ module Rawg
         url: "#{BASE_URL}#{endpoint}?key=#{TOKEN}#{options.to_query.prepend("&") unless options.empty?}",
         headers: {"Content-Type" => "application/json"}
       )
-      {code: result.code, status: "Success", data: JSON.parse(result.body)}
+      {code: result.code, status: "Success", data: self.build_data(result.body)}
     rescue RestClient::ExceptionWithResponse => error
       {code: error.http_code, status: error.message, data: Errors.map(error.http_code)}
+    end
+
+    private
+    def self.build_data body
+      json = JSON.parse(body)
+      json.except!("next", "previous")
     end
   end
 end
